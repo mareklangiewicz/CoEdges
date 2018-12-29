1e3321d09a73af4dd0b31ec548cdec4fa43f62be
Marek Langiewicz <marek.langiewicz@gmail.com>
2018-12-29 22:04:43 +0100

    Remove rx stuff

---
+++ src/test/kotlin/pl/mareklangiewicz/coedges/LaunchTest.kt
@@ -0,0 +1,18 @@
+package pl.mareklangiewicz.coedges
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import pl.mareklangiewicz.uspek.USpekRunner
+import pl.mareklangiewicz.uspek.o
+import pl.mareklangiewicz.uspek.uspek
+
+@RunWith(USpekRunner::class)
+class LaunchTest {
+
+    @Test
+    fun uspek() = uspek {
+        "On launch" o {
+            // TODO
+        }
+    }
+}
