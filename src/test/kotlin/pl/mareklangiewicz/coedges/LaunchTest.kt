68e87970984fed088c99652b7a5a0625bd30ccc9
Marek Langiewicz <marek.langiewicz@gmail.com>
2018-12-30 00:57:23 +0100

    Add some ad hoc cancellation tests

--- src/test/kotlin/pl/mareklangiewicz/coedges/LaunchTest.kt
+++ src/test/kotlin/pl/mareklangiewicz/coedges/LaunchTest.kt
@@ -14,9 +14,9 @@ class LaunchTest {
 
     @Test
     fun uspek() = uspek {
-        val done = mutableSetOf<String>()
         "On launch" o {
             runBlocking {
+                val done = mutableSetOf<String>()
                 launch {
                     done += "started"
                     delay(500)
@@ -29,6 +29,32 @@ class LaunchTest {
                 "only started after 490ms" o { assert(done.size == 1) }
                 delay(10)
                 "ended after 500ms" o { assert("ended" in done) }
+            }
+        }
+
+        "On cancellation" o {
+            runBlocking {
+                val done = mutableSetOf<String>()
+                val job = launch {
+                    repeat(10) {
+                        done += "iteration ${it+1}"
+                        delay(50)
+                    }
+                }
+                "not started yet" o { assert(done.isEmpty()) }
+                delay(1)
+                "started first iteration" o { assert("iteration 1" in done) }
+                "did not start second iteration" o { assert("iteration 2" !in done) }
+                delay(190)
+                "started 4 iterations" o {
+                    assert("iteration 4" in done)
+                    assert("iteration 5" !in done)
+                }
+                job.cancel()
+                delay(300)
+                "did not start fifth iteration" o {
+                    assert("iteration 5" !in done)
+                }
             }
         }
     }
